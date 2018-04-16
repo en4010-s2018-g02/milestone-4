d353a7505f1c859f820dca03ac2e385cb133991c
Nicholas Alexander Gacharich <35305711+ngacharich2014@users.noreply.github.com>
2018-04-16 15:57:24 -0400

    Add files via upload

---
+++ lab_info.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST['submit'])){
+    $servername="lamp.eng.fau.edu";
+    $username="CEN4010_S2018g02";
+    $password="cen4010_s2018";
+    $db="CEN4010_S2018g02";
+
+    $conn =  mysqli_connect($servername,$username,$password);
+
+    if (mysqli_connect_error()) {
+        die("Database connection failed: " . mysqli_connect_error());
+    }
+    mysqli_select_db($conn,"CEN4010_S2018g02");
+
+    $sql="INSERT INTO module3(znumber,classname) VALUES ('$_POST[znumber]','$_POST[classname]')";
+
+    mysqli_query($conn,"INSERT INTO module3(znumber,classname) VALUES ('$_POST[znumber]','$_POST[classname]')");
+   
+    mysqli_close($conn);
+    sleep(5);
+    header('location: http://lamp.cse.fau.edu/~CEN4010_S2018g02/module3/index.html');
+    exit;
+  }
+?>
