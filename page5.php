d55889c2ccba2d3d4139481e5e91522d3a957a27
Nicholas Alexander Gacharich <35305711+ngacharich2014@users.noreply.github.com>
2018-04-23 15:57:52 -0400

    Update and rename lab_info.php to page5.php

--- page5.php
+++ page5.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $servername="lamp.eng.fau.edu";
+    $servername="lamp.cse.fau.edu";
     $username="CEN4010_S2018g02";
     $password="cen4010_s2018";
     $db="CEN4010_S2018g02";
@@ -12,13 +12,13 @@ if(isset($_POST['submit'])){
     }
     mysqli_select_db($conn,"CEN4010_S2018g02");
 
-    $sql="INSERT INTO module3(name,znumber,class_crn,classname) VALUES ('$_POST[name]','$_POST[znumber]','$_POST[class_crn]','$_POST[classname]')";
+    $sql="INSERT INTO module3(znumber,classname) VALUES ('$_REQUEST[znumber]','$_REQUEST[classname]')";
 
-    mysqli_query($conn,"INSERT INTO module3(name,znumber,class_crn,classname) VALUES ('$_POST[name]','$_POST[znumber]','$_POST[class_crn]','$_POST[classname]')");
-   
+    mysqli_query($conn,"INSERT INTO module3(znumber,classname) VALUES ('$_REQUEST[znumber]','$_REQUEST[classname]')");
+
     mysqli_close($conn);
     sleep(5);
     header('location: http://lamp.cse.fau.edu/~CEN4010_S2018g02/module3/index.html');
     exit;
-  }
+}
 ?>
